68cd60a9edc7a6f3782f08ee83e37866399bd0d5
kevin226j <kevin226j@gmail.com>
2018-12-28 17:05:24 -0800

    rest api created

--- lib/app.ts
+++ lib/app.ts
@@ -1,12 +1,19 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as mongoose from 'mongoose'
+import {Routes} from './routes'
 
 class App {
+    public mongoUrl : string = 'mongodb://localhost/ecommerceDB';
     public app: express.Application;
+    public routePrv : Routes = new Routes();
+
 
     constructor() {
         this.app = express();
         this.config();
+        this.mongoSetUp();
+        this.routePrv.routes(this.app);
     }
 
     private config(): void {
@@ -15,6 +22,11 @@ class App {
 
         ////support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({extended: false}));
+    }
+
+    private mongoSetUp(): void {
+        (<any>mongoose).Promise = global.Promise;
+        mongoose.connect(this.mongoUrl, {useNewUrlParser: true}).then(()=>console.log('DB connected')).catch(err => console.log(err));
     }
 
 }
--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,21 @@
 import app from './app'
+import * as https from 'https';
+import * as fs from 'fs'
+
 const {PORT = 3000} = process.env
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+const httpsOptions = {
+    key: fs.readFileSync('./config/key.pem', 'utf8'),
+    cert: fs.readFileSync('./config/server.crt', 'utf8')
+}
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port: ${PORT}`)
 })
+
+/* For testing with https */
+/*
+https.createServer(httpsOptions, app).listen(PORT, ()=>{
+     console.log(`Server listening on port: ${PORT}`)
+})
+*/
